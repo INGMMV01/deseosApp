e75aa91d9c0367b4b00d4a38b47a3708625df7b3
kitos <kitos78@gmail.com>
2018-05-05 07:30:55 +0200

    video 112

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,29 +3,40 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
+/*
 import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
-import { HomePage } from '../pages/home/home';
+import { HomePage } from '../pages/home/home';ç
+*/
 import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+// servicios
 import { ListaDeseosService } from './services/lista-deseos.service';
 
-
+// components
 import { PendientesComponent } from './../pages/pendientes/pendientes.component';
 import { TerminadosComponent } from './../pages/terminados/terminados.component';
+import { AgregarComponent } from './../pages/agregar/agregar.component';
+
+// pipes
+import { PlaceholderPipe } from './../app/pipes/placeholder.pipe';
+
 
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
+    PlaceholderPipe,
+    /*AboutPage,
     ContactPage,
-    HomePage,
+    HomePage,*/
+    TabsPage,
     PendientesComponent,
     TerminadosComponent,
-    TabsPage
+    AgregarComponent,
+    
   ],
   imports: [
     BrowserModule,
@@ -34,12 +45,13 @@ import { TerminadosComponent } from './../pages/terminados/terminados.component'
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
+    /*AboutPage,
     ContactPage,
-    HomePage,
+    HomePage,*/
     TabsPage,
     PendientesComponent,
-    TerminadosComponent
+    TerminadosComponent,
+    AgregarComponent,
   ],
   providers: [
     StatusBar,
